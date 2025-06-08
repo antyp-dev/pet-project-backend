7f499557ec8700ef9f660ff72d95eb7758b7ccb6
user <antyp.dev@gmail.com>
2025-06-09 01:20:42 +0300

    ID-B-8

---
+++ PetPlatform.Backend/src/PetPlatform.API/Response/Envelope.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+using PetPlatform.Domain.Shared;
+
+namespace PetPlatform.API.Response;
+
+public record Envelope
+{
+    public object? Result { get; }
+    public ErrorList? Errors { get; }
+    public bool IsError => Errors != null || (Errors != null && Errors.Any());
+    public DateTime TimeGenerated { get; }
+
+    [JsonConstructor]
+    private Envelope(object? result, ErrorList? errors)
+    {
+        Result = result;
+        Errors = errors;
+        TimeGenerated = DateTime.Now;
+    }
+
+    public static Envelope Ok(object? result = null) =>
+        new(result, null);
+
+    public static Envelope Error(ErrorList errors) =>
+        new(null, errors);
+}
+
+public record Envelope<T>
+{
+    public T? Result { get; }
+    public ErrorList? Errors { get; }
+    public bool IsError => Errors != null || (Errors != null && Errors.Any());
+    public DateTime TimeGenerated { get; }
+
+    [JsonConstructor]
+    private Envelope(T? result, ErrorList? errors)
+    {
+        Result = result;
+        Errors = errors;
+        TimeGenerated = DateTime.Now;
+    }
+
+    public static Envelope<T> Ok(T? result = default) =>
+        new(result, null);
+
+    public static Envelope<T> Error(ErrorList errors) =>
+        new(default, errors);
+}
--- PetPlatform.Backend/src/PetPlatform.Domain/Shared/Error.cs
+++ PetPlatform.Backend/src/PetPlatform.Domain/Shared/Error.cs
@@ -3,27 +3,32 @@
 public record Error
 {
     public const string SEPARATOR = "||";
-    
+
     public string Code { get; }
     public string Message { get; }
     public ErrorType Type { get; }
+    public string? InvalidField { get; }
 
-    private Error(string code, string message, ErrorType type)
+    private Error(string code, string message, ErrorType type, string? invalidField = null)
     {
         Code = code;
         Message = message;
         Type = type;
+        InvalidField = invalidField;
     }
-    
-    public static Error Validation(string code, string message) => new(code, message, ErrorType.Validation);
+
+    public static Error Validation(string code, string message, string? invalidField = null)
+        => new(code, message, ErrorType.Validation, invalidField);
+
     public static Error NotFound(string code, string message) => new(code, message, ErrorType.NotFound);
     public static Error Failure(string code, string message) => new(code, message, ErrorType.Failure);
     public static Error Conflict(string code, string message) => new(code, message, ErrorType.Conflict);
-    
+
     public string Serialize()
     {
         return string.Join(SEPARATOR, Code, Message, Type);
     }
+
     public static Error Deserialize(string serialized)
     {
         var parts = serialized.Split(SEPARATOR);
@@ -40,6 +45,8 @@ public record Error
 
         return new Error(parts[0], parts[1], type);
     }
+    
+    public ErrorList ToErrorList() => new([this]);
 }
 
 public enum ErrorType
